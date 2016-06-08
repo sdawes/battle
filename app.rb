66a69b2bde131bf07c34aa8cc0b0ab595516c3f5
a-mellor <alexander.mellor@gmail.com>
2016-06-08 14:32:55 +0100

    updated app

--- app.rb
+++ app.rb
@@ -18,6 +18,6 @@ class Battle < Sinatra::Base
     @player_2_name = session[:player_2_name]
     erb :play
   end
-  # start the server if ruby file executed directly
+  # start the server if ruby file executed directly ££££
   run! if app_file == $0
 end
