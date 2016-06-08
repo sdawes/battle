8eb50521d24839390e69164d42ec6764fefc092b
a-mellor <alexander.mellor@gmail.com>
2016-06-08 15:56:03 +0100

    helpers

--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -1,10 +1,7 @@
 
 feature 'Hit Points' do
   scenario "show Player 2's hitpoint" do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Mittens 60"
   end
 end
