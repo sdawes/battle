f90a80c9cbde5ee451630b7cb27380de58a27bec
Carlos <chdezmar@gmail.com>
2016-06-08 12:37:46 +0100

    Adding a session

--- app.rb
+++ app.rb
@@ -1,17 +1,28 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'Secret Session'
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+    end
+
+  get "/play" do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb :play
   end
 
-  
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
