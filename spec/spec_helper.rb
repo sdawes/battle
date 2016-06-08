ffab88177f504c5e7762da42f26584a315ac7a1b
a-mellor <alexander.mellor@gmail.com>
2016-06-08 15:57:55 +0100

    fixed helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-require 'web_helpers'
+require_relative './features/web_helpers.rb'
 
 
 Capybara.app = Battle
