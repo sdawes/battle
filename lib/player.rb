a89a61ecf98c7860ec401ad5b23a666baef98048
Carlos <chdezmar@gmail.com>
2016-06-08 17:32:38 +0100

    Single Responsability Principle when attacking

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,20 @@
 class Player
 
+  DEFAULT_HIT_POINTS = 60
+  ATTACK_DAMAGE = 10
+
   attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
-    @hit_points = 60
+    @hit_points = DEFAULT_HIT_POINTS
   end
 
+  def receive_damage
+    @hit_points -= ATTACK_DAMAGE
+  end
 
-
+  def attack(other_player)
+    other_player.receive_damage
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,8 +2,32 @@ require 'player'
 
 describe Player do
   subject(:player) { Player.new("Steve") }
+  subject(:player2) { Player.new("Carlos") }
 
+describe "#name" do
   it 'returns players name' do
     expect(player.name).to eq("Steve")
   end
+end
+
+describe "#hit_points" do
+  it 'returns hit points' do
+    expect(player.hit_points).to eq(Player::DEFAULT_HIT_POINTS)
+  end
+end
+
+describe "#attack" do
+  it 'attacks player' do
+    player.attack(player2)
+    expect(player2).to respond_to(:receive_damage)
+
+  end
+end
+
+describe "#receive_damage" do
+  it 'reduces HP by Damage' do
+    expect { player.receive_damage }.to change{player.hit_points}.by(-10)
+  end
+end
+
 end
